38d663f635cc7b02b384fda866c8bdb072adaa66
rdbende <rdbende@gmail.com>
2022-08-30 19:32:57 +0200

    Add more metadata

--- setup.py
+++ setup.py
@@ -7,6 +7,18 @@ platform = {"darwin": "mac", "win32": "win"}.get(sys.platform, "unix")
 
 setup(
     name=f"libtukaan-{platform}",
+    version="0.1.2",
+    license="MIT",
+    author="rdbende",
+    author_email="rdbende@gmail.com",
+    description="Binary extensions for Tukaan",
+    url="https://tukaan.github.io",
+    project_urls={
+        "Documentation": "https://tukaan.github.io/docs",
+        "Source": "https://github.com/tukaan/libtukaan",
+        "Tracker": "https://github.com/tukaan/libtukaan/issues",
+    },
+    python_requires=">=3.7",
     packages=["libtukaan"],
     package_data={"libtukaan": [f"{platform}/*.*"]},
 )
