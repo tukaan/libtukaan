211b9668e83784e649ed79f3c1b4561bf0448fe7
rdbende <rdbende@proton.me>
2023-04-30 11:58:24 +0200

    Fixenings

--- libtukaan/xcursor.py
+++ libtukaan/xcursor.py
@@ -7,8 +7,8 @@ from tukaan._tcl import Tcl
 
 
 class Xcursor:
-    loaded_cursors: set[int] = set()
-    defined_cursors: dict[str, int] = {}
+    loaded_cursors: dict[str, str] = {}
+    defined_cursors: dict[str, str] = {}
 
     @staticmethod
     def init() -> None:
@@ -20,22 +20,22 @@ class Xcursor:
         )
 
     @classmethod
-    def load_cursor(cls, source: Path) -> int:
+    def load_cursor(cls, source: Path) -> str:
         if not source.exists():
             raise FileNotFoundError(source)
 
-        cursor_id = Tcl.call(int, "Xcursor::load_cursor_file", source)
-        cls.loaded_cursors.add(cursor_id)
+        cursor_id = Tcl.call(str, "Xcursor::load_cursor_file", source)
+        cls.loaded_cursors[cursor_id] = str(source.resolve().absolute())
         return cursor_id
 
     @classmethod
-    def set_cursor(cls, widget_name: str, cursor_id: int) -> None:
+    def set_cursor(cls, widget_name: str, cursor_id: str) -> None:
         Tcl.call(None, "Xcursor::set_cursor", widget_name, cursor_id)
-        cls.defined_cursors[widget_name] = cursor_id
+        cls.defined_cursors[cursor_id] = widget_name
 
     @classmethod
     def cleanup_cursors(cls) -> None:
-        for widget_name in cls.defined_cursors:
+        for widget_name in cls.defined_cursors.values():
             Tcl.call(None, "Xcursor::undefine_cursor", widget_name)
         for cursor_id in cls.loaded_cursors:
             Tcl.call(None, "Xcursor::free_cursor", cursor_id)
